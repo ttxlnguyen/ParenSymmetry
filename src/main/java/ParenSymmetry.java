3ff815630ebeab97647b2fac7a562fe2a5146fba
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-07-04 08:48:05 -0400

    added balance logic

--- src/main/java/ParenSymmetry.java
+++ src/main/java/ParenSymmetry.java
@@ -1,16 +1,31 @@
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.io.IOException;
 
 public class ParenSymmetry {
 
 
-    private Boolean isBalanced(String s) {
+    public Boolean isBalanced(String stringtoTest) {
         // implement this method
-        return null;
+        int balanced = 0;
+
+        for(int i = 0; i <stringtoTest.length(); i++){
+            char currentChar = stringtoTest.charAt(i);
+
+            if(currentChar == '('){
+                balanced++;
+            } else if (currentChar == ')') {
+                balanced--;
+            }
+        }
+        return balanced == 0;
     }
 
-    private void checkFile(String filename) {
+    public void checkFile(String filename) {
         // open file named filename
 
-        // for each line in the file
+            // for each line in the file
             // read the line
             // print whether or not the line's parenthesis are balanced
 
